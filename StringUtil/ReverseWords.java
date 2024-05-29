04bba8817fc4e073899c681077e55c343d1e8ae8
kenny <958580018@qq.com>
2024-05-29 22:25:22 +0800

    update: 151.翻转字符串里的单词

---
+++ StringUtil/ReverseWords.java
@@ -0,0 +1,8 @@
+package StringUtil;
+
+public class ReverseWords {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.reverseWords("  hello world  "));
+    }
+}
--- StringUtil/Solution.java
+++ StringUtil/Solution.java
@@ -45,4 +45,40 @@ public class Solution {
         }
     }
 
+    public String reverseWords(String s) {
+        char[] chars = s.toCharArray();
+        StringBuilder stringBuilder = removeSpace(s);
+        chars = stringBuilder.toString().toCharArray();
+        // 反转
+        reverseStringUse(chars, 0, stringBuilder.length() - 1);
+        int left = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == ' ') {
+                reverseStringUse(chars, left, i - 1);
+                left = i + 1;
+            }
+        }
+        if (left < chars.length) {
+            reverseStringUse(chars, left, chars.length - 1);
+        }
+        return new String(chars);
+    }
+
+    private StringBuilder removeSpace(String s) {
+        // System.out.println("ReverseWords.removeSpace() called with: s = [" + s + "]");
+        int start = 0;
+        int end = s.length() - 1;
+        while (s.charAt(start) == ' ') start++;
+        while (s.charAt(end) == ' ') end--;
+        StringBuilder sb = new StringBuilder();
+        while (start <= end) {
+            char c = s.charAt(start);
+            if (c != ' ' || sb.charAt(sb.length() - 1) != ' ') {
+                sb.append(c);
+            }
+            start++;
+        }
+        return sb;
+    }
+
 }
